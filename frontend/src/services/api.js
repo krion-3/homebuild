9654bbb81c55388f03e59003f403319646d102e1
krion-3 <krion36@gmail.com>
2026-02-28 18:33:46 +0300

    local host changed to allow mobile login

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://192.168.8.8:5000/api',
   headers: {
     'ngrok-skip-browser-warning': 'true'
   }
